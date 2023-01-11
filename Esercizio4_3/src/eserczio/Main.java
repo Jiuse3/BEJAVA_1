83b83874cb10bed34580acad5a5a060cb0116801
Jiuse3 <114151083+Jiuse3@users.noreply.github.com>
2023-01-11 16:14:35 +0000

    .

---
+++ Esercizio4_3/src/eserczio/Main.java
@@ -0,0 +1,20 @@
+package eserczio;
+
+import java.util.Scanner;
+
+public class Main {
+
+	 public static void main(String[] args) {
+	        Scanner input = new Scanner(System.in);
+	        System.out.println("Inserisci un numero");
+
+	         int num = input.nextInt();
+	         for (int i = num -1; i >= 0; --i) { 
+	     System.out.println( i );
+	    }
+
+	}
+	}
+
+
+
---
+++ Esercizio4_3/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author rokiseydi
+ *
+ */
+module Esercizio4_3 {
+}
